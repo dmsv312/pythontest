8149a0d322982acbfa6e0be39bb203f1a083493c
dv_ma <dm.sv312@gmail.com>
2024-05-21 21:03:09 +0700

    update

--- squid_axl_usdc.py
+++ squid_axl_usdc.py
@@ -1,59 +1,61 @@
 import pyautogui
 from time import sleep
 import requests
+import random
 # response = requests.get('http://127.0.0.1:8000/api/v1/next-transaction')
 # x = response.json()
 # amount = x["amount"]
-amount = random.randrange(0.5, 1.5, 0.1)
+amount = random.randrange(5, 15, 1)/10
 pyautogui.PAUSE = 2
 sleep(1)
 # open chrome
 pyautogui.moveTo(208, 13, 3)
 pyautogui.click()
 # print site
-pyautogui.moveTo(171, 76, 2)
+pyautogui.moveTo(312, 115, 2)
 pyautogui.click()
 pyautogui.write('https://app.squidrouter.com')
+sleep(1)
 pyautogui.press('enter')
 sleep(5)
 # pick axl
-pyautogui.moveTo(696, 419, 2)
+pyautogui.moveTo(696, 459, 2)
 pyautogui.click()
 axelar = 'axe'
 for char in axelar:
     pyautogui.write(char)
     sleep(1)
-pyautogui.moveTo(670, 560, 1)
+pyautogui.moveTo(670, 590, 1)
 pyautogui.click()
 sleep(7)
 # pick usdc
-pyautogui.moveTo(703, 609, 2)
+pyautogui.moveTo(703, 649, 2)
 pyautogui.click()
 axelar = 'axe'
 for char in axelar:
     pyautogui.write(char)
     sleep(1)
-pyautogui.moveTo(670, 560, 1)
+pyautogui.moveTo(670, 590, 1)
 pyautogui.click()
 sleep(7)
-pyautogui.moveTo(867, 603, 1)
+pyautogui.moveTo(867, 643, 1)
 pyautogui.click()
 usdc = 'axlu'
 for char in usdc:
     pyautogui.write(char)
     sleep(1)
-pyautogui.moveTo(656, 489, 1)
+pyautogui.moveTo(656, 528, 1)
 pyautogui.click()
 sleep(7)
 # connect wallet
-pyautogui.moveTo(928, 361, 1)
+pyautogui.moveTo(928, 401, 1)
 pyautogui.click()
 sleep(1)
-pyautogui.moveTo(667, 451, 1)
+pyautogui.moveTo(667, 491, 1)
 pyautogui.click()
 sleep(20)
 # go to value field and write amount from back
-pyautogui.moveTo(646, 462, 2)
+pyautogui.moveTo(646, 502, 2)
 pyautogui.click()
 # enter by symbols
 newamount = str(amount)
@@ -62,7 +64,7 @@ for char in newamount:
     sleep(1)
 sleep(3)
 # swap
-pyautogui.moveTo(792, 890, 1)
+pyautogui.moveTo(792, 930, 1)
 pyautogui.click()
 sleep(10)
 # confirm in metamask
@@ -93,5 +95,9 @@ sleep(3)
 
 # sleep(3)
 # close mozilla
-pyautogui.moveTo(1587, 32, 3)
+pyautogui.moveTo(1591, 42, 3)
+pyautogui.click()
+sleep(1)
+# go to 1 screen
+pyautogui.moveTo(1125, 14, 1)
 pyautogui.click()
